70983dcf33d36552fc8ba9059127ddc6ec48f434
Skyllerz-IT <dadivarie@gmail.com>
2024-11-04 11:24:28 +0100

    Davide Was Here

--- challenge2/ContentView.swift
+++ challenge2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Davide Was Here")
         }
         .padding()
     }
