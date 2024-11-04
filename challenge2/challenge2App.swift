1c428ae2434aec8cf2e75445e8f16e414ec3c4e6
francescafinetti <francy.fine@icloud.com>
2024-11-04 10:34:13 +0100

    Initial Commit

---
+++ challenge2/challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge2App.swift
+//  challenge2
+//
+//  Created by Francesca Finetti on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
