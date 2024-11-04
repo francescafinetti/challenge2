caa11f748a9b80891f69263ccd7b285c1fa7b0a9
francescafinetti <francy.fine@icloud.com>
2024-11-04 11:31:01 +0100

    ciao

--- challenge2/ContentView.swift
+++ challenge2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Davide Was Here")
+            Text("uaaaaa")
         }
         .padding()
     }
