85eb7f430256879f0bdb71f81d227eda397a96b4
Skyllerz-IT <dadivarie@gmail.com>
2024-11-07 11:08:32 +0100

    tab bar

---
+++ challenge2/RecentGamesView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentGamesView.swift
+//  challenge2
+//
+//  Created by Davide Picentino on 07/11/24.
+//
+
+import SwiftUI
+
+struct RecentGamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentGamesView()
+}
