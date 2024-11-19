b825362e57ee3f26776205c66ab8c21ae6e233b7
Skyllerz-IT <dadivarie@gmail.com>
2024-11-19 09:05:05 +0100

    troppa roba

---
+++ challenge2/ContentView/DarkMode.swift
@@ -0,0 +1,18 @@
+//
+//  DarkMode.swift
+//  challenge2
+//
+//  Created by Davide Picentino on 18/11/24.
+//
+
+import SwiftUI
+
+struct DarkMode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DarkMode()
+}
---
+++ challenge2/ImagePickerModel.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import PhotosUI
+
+struct ImagePickerModel: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePickerModel
+
+        init(_ parent: ImagePickerModel) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+
+            guard let provider = results.first?.itemProvider, provider.canLoadObject(ofClass: UIImage.self) else { return }
+
+            provider.loadObject(ofClass: UIImage.self) { image, _ in
+                self.parent.image = image as? UIImage
+            }
+        }
+    }
+}
